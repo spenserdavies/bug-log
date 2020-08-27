af627a52d7c937bc37d52bd51000828d41cd27f1
Spenser <spenser.davies@gmail.com>
2020-08-27 13:41:09 -0600

    building out profile page

--- clientsrc/src/Pages/Profile.vue
+++ clientsrc/src/Pages/Profile.vue
@@ -1,9 +1,43 @@
 <template>
-  <div class="about text-center">
-    <h1 class="mt-3">Welcome To Your Profile!</h1>
-    <h3 class="mt-3">{{ profile.name }}</h3>
-    <img class="rounded mt-3" :src="profile.picture" alt="" />
-    <p>{{ profile.email }}</p>
+  <div class="container bg-light shadow mt-3">
+    <div class="row">
+      <div class="col-12 mt-3">
+        <p>This is your profile page! Here you can see all of the bugs you have reported!</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12 mt-3">
+        <p>The email associated with your account is: <b>{{ profile.email}}</b></p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12 mt-5">
+        <p>Your bugs:</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-4">
+        <h5>Title</h5>
+      </div>
+      <div class="col-6">
+        <h5>Description</h5>
+      </div>
+      <div class="col-2 text-center">
+        <h5>Status</h5>
+      </div>
+    </div>
+    <div class="row d-flex border-top" v-for="bug in bugs" :key="bug.id">
+      <div class="col-4 py-2 border-right">
+        <p> {{ bug.title }}</p>
+      </div>
+      <div class="col-6 py-2 border-right">
+        <p> {{bug.description}}</p>
+      </div>
+      <div class="col-2 py-2 text-center">
+        <p v-if="bug.closed" class="text-danger">CLOSED</p>
+        <p v-else class="text-success">OPEN</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,6 +47,9 @@ export default {
   computed: {
     profile() {
       return this.$store.state.profile;
+    },
+    bugs(){
+      return this.$store.state.bugs.filter(b => b.creatorEmail == this.profile.email);
     }
   }
 };
